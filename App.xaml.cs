80c8aaf062944954579b82aad698d2b6f4c7413f
saros <saros@LAPTOP-JQTANCVI>
2022-12-19 21:32:28 +0200

    Laborator 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
-﻿namespace Sarosi_Lucian_Lab7;
+﻿using System;
+using Sarosi_Lucian_Lab7;
+using System.IO;
+using Sarosi_Lucian_Lab7.Data;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database 
+	{ 
+		get 
+		{ 
+			if (database == null) 
+			{ 
+				database = new 
+					ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+					LocalApplicationData), "ShoppingList.db3")); 
+			} 
+			return database; 
+		} 
+	}
+    public App()
 	{
 		InitializeComponent();
 
